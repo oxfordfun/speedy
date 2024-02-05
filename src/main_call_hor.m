78455fad658d7586e4cecfa1b9854377d5537062
abuts <Alex.Buts@stfc.ac.uk>
2024-02-05 12:57:37 +0000

    Re #1 added wkc26243 path to test files

--- src/main_call_hor.m
+++ src/main_call_hor.m
@@ -2,6 +2,7 @@
 file_base = 'STO_300K_35meV.sqw'; %(178G)
 file_base2 = 'STO_300K_35meV_h4.sqw'; %(91G)
 file_base3 = 'STO_400K_10.1meV.sqw'; %(63Gb);
+%data_path = '/instrument/MERLIN/RBNumber/RB2220429/sqw';
 data_path = '/mnt/ceph/home/y1113254/Horace_test/sqw';
 
 filename = fullfile(data_path,file_base3);
